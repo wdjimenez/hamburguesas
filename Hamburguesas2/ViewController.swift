f4a1c017f3be27cf05e6cce2cc2f89c437d13817
Willians Dario Jimenez Andrade <dariojimenez@MacBookProWill.local>
2016-11-30 18:15:02 -0600

    se actualizan los archivos

--- Hamburguesas2/ViewController.swift
+++ Hamburguesas2/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var paises = ColeccionDePaises()
+    var hamburguesas = ColeccionDeHamburguesas()
+    
+    @IBOutlet weak var etiquetaPais: UILabel!
+    @IBOutlet weak var etiquetaHamburguesa: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func ObtenerPaisHamburguesa() {
+        let r = CGFloat((Double(arc4random()) % 255) / 255.0)
+        let g = CGFloat((Double(arc4random()) % 255) / 255.0)
+        let b = CGFloat((Double(arc4random()) % 255) / 255.0)
+        
+        let color = UIColor(red: r, green: g, blue: b, alpha: 1)
+        
+        view.backgroundColor = color
+        view.tintColor =  color
+
+        
+        etiquetaPais.text = paises.obtenPais()
+        etiquetaHamburguesa.text = hamburguesas.obtenHamburguesas()
+    }
 
 }
 
